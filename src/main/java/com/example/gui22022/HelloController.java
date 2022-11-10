47aefdb26860bc23a0793f79ce7ea8cbf5885571
mcelrea <mcelrea@damien-hs.edu>
2022-11-10 10:49:19 -0800

    Added AnimationTimer for Canvas animation and began the character sheet tab.

--- src/main/java/com/example/gui22022/HelloController.java
+++ src/main/java/com/example/gui22022/HelloController.java
@@ -4,7 +4,9 @@ import javafx.animation.AnimationTimer;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
@@ -13,7 +15,13 @@ public class HelloController {
     @FXML
     private Label welcomeText;
     @FXML
+    private Button editNameButton;
+    @FXML
+    private TextField nameField;
+    @FXML
     private Canvas gameCanvas;
+    @FXML
+    private Label nameLabel;
 
     int x=100, y=100;
 
@@ -22,21 +30,41 @@ public class HelloController {
         GraphicsContext gc = gameCanvas.getGraphicsContext2D();
         Font theFont = Font.font( "Times New Roman", FontWeight.BOLD, 48 );
         gc.setFont( theFont );
+        nameField.setEditable(false);
+        nameField.setVisible(false);
 
-        new AnimationTimer() {
+        AnimationTimer anim = new AnimationTimer() {
             @Override
             public void handle(long l) {
                 gc.clearRect(0,0,800,600);
                 gc.setFill( Color.RED );
                 gc.setStroke( Color.BLACK );
                 gc.setLineWidth(2);
-                x++;
-                y++;
-                gc.fillText( "Hello, World!", x, y );
+                gc.fillText( "No Character", x, y );
+                gc.fillText( "Create Character", x, y+100 );
             }
-        }.start();
+        };
 
+        anim.start();
 
+    }
+
+    @FXML
+    protected void setEditNameButtonClick() {
+        String buttonText = editNameButton.getText();
+        if(buttonText.equals("Edit")) {
+            editNameButton.setText("Save");
+            nameLabel.setVisible(false);
+            nameField.setVisible(true);
+            nameField.setEditable(true);
+        }
+        else if(buttonText.equals("Save")) {
+            editNameButton.setText("Edit");
+            nameLabel.setVisible(true);
+            nameField.setVisible(false);
+            nameField.setEditable(false);
+            nameLabel.setText(nameField.getText());
+        }
     }
 
     @FXML
